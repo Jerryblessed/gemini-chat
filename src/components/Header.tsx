3e7c434a66eeca537a08d09089b1d2be43c4dcad
Jeremiah Ope <106873857+Jerryblessed@users.noreply.github.com>
2024-08-10 12:06:02 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Header = ({ name }: Props) => {
   return (
     <div className="position fixed top-2 w-full z-10 ">
-      <h1 className="text-2xl md:text-4xl font-semibold bg-gradient-to-br from-slate-50 via-slate-100 to-indigo-400 text-center bg-clip-text text-transparent px-4 py-1 rounded-full border w-fit mx-auto border-slate-500/60 backdrop-blur shadow-md">
+      <h3 className="text-2xl md:text-4xl font-semibold bg-gradient-to-br from-slate-50 via-slate-100 to-indigo-400 text-center bg-clip-text text-transparent px-4 py-1 rounded-full border w-fit mx-auto border-slate-500/60 backdrop-blur shadow-md">
         Powered by Google Gemini {name}{" "}
         {name === "Pro" ? (
           <Bot color="white" size={35} className="inline-flex" />
@@ -16,7 +16,7 @@ const Header = ({ name }: Props) => {
           <Eye color="white" size={35} className="inline-flex" />
         )}
         ✨
-      </h1>
+      </h3>
     </div>
   );
 };
